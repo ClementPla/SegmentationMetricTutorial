a8e3ca667ecbd36a5d836422e92cd44c79719c0d
Emmanuelle Richer <emmanuelle.richer@hotmail.com>
2022-07-30 13:45:14 -0400

    improvement in schematics of metrics, addition of boundary dice and iou tooltips

--- src/app/Components/tooltips/tooltips.component.ts
+++ src/app/Components/tooltips/tooltips.component.ts
@@ -8,16 +8,20 @@ import { ControlUIService } from 'src/app/Services/control-ui.service';
 })
 export class TooltipsComponent implements OnInit {
 
-  @Input() type:string
-  precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
+  @Input() type: string
+  precision = '$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
+  accuracy = '$$ \\text{accuracy} = \\frac{TP + TN}{TP + TN + FP + FN} $$'
+  specificity = '$$ \\text{specificity} = \\frac{TN}{TN+FP} $$'
+  sensitivity = '$$ \\text{sensitivity} = \\frac{TP}{TP+FN} $$'
 
-  accuracy='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
-  /* precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
-  precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
-  precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
-  precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'
-  precision='$$ \\text{precision} = \\frac{TP}{TP+FP} $$'*/
-  constructor(public controlUIService:ControlUIService) { }
+  iou = '\\begin{align*} \\text{IoU} &= \\frac{|P \\cap G| }{ |P \\cup G|} \\\\ \\text{IoU} &= \\frac{TP}{TP + FP + TN} \\end{align*}'
+  dice = '\\begin{align*} \\text{dice} &= \\frac{2 \\cdot TP}{2 \\cdot TP + FP + FN} \\\\ \\text{dice} &= \\frac{2 \\cdot |P \\cap G|}{|P| + |G|} \\end{align*}'
+  kappa = '$$ \\kappa = \\frac{p_{agree} - p_{chance}}{1 - p_{chance}}$$'
+
+  boundaryIoU = '$$ \\text{boundary IoU} = \\frac{|(P_d \\cap P) \\cap (G_d \\cap G)|}{|(P_d \\cap P) \\cup (G_d \\cap G)|}$$'
+  boundaryDice = '$$ \\text{dice}= \\frac{2 \\cdot |(P_d \\cap P) \\cap (G_d \\cap G)|}{|(P_d \\cap P))| + |(G_d \\cap G))|} $$'
+
+  constructor(public controlUIService: ControlUIService) { }
 
   ngOnInit(): void {
   }
